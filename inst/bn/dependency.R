82de99042785395896176386138d0e6c898cc227
shubham14101 <shubham14101@iiitd.ac.in>
2019-04-19 21:30:59 +0530

    removed redundancy

--- inst/bn/dependency.R
+++ inst/bn/dependency.R
@@ -23,7 +23,6 @@ dependency<-function()
     install.packages('gRain')
   }
   if(!"RCy3" %in% installed.packages()){
-    install.packages("BiocManager")
     BiocManager::install("RCy3")
   }
   library(RCy3)
